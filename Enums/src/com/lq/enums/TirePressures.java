ccfbde99acd16f32aebe231accaf1b922db91fc6
dhauss <davidkhauss@gmail.com>
2023-01-11 17:16:50 -0500

    Enums final

--- Enums/src/com/lq/enums/TirePressures.java
+++ Enums/src/com/lq/enums/TirePressures.java
@@ -3,11 +3,6 @@ package com.lq.enums;
 public enum TirePressures {
 	LF(30, "Left Front"), RF(30, "Right Front"), LR(32, "Left Back"), RR(32, "Left Back");
 	
-	private TirePressures(int pressure) {
-		this.pressure = pressure;
-		this.name = "Unknown";
-	}
-	
 	private TirePressures(int pressure, String name) {
 		this.pressure = pressure;
 		this.name = name;		
